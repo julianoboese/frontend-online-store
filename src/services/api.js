e5a731b07a8264fb4ce5e740771811a03b8c532f
Juliano Boese <juliano.boese@gmail.com>
2022-02-09 16:08:51 -0300

    Requisito 1 - Implementa requisições à API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const json = await response.json();
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // if (categoryId && query) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const json = await response.json();
+  return json;
+  // } if (categoryId) {
+  //   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  //   const json = await response.json();
+  //   return json;
+  // }
+  // const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  // const json = await response.json();
+  // return json;
 }
