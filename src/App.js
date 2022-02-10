3222358438980d3e1053b6590ef2716b173159bd
Hiago Rodrigues <61169644+Hiago-Vitor@users.noreply.github.com>
2022-02-09 21:53:30 -0300

    Revert "[Main group 6] requisito 3"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Cart from './pages/Cart';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/cart" component={ Cart } />
+      <div className="App">
+        <Home />
+      </div>
     </Router>
   );
 }
