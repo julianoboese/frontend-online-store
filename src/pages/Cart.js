290f0a232a6731e7a68162ca325f5c2b80664c78
Ingrid_Ramos <ingridrcs@gmail.com>
2022-02-11 16:52:24 -0300

    requisito 10 concluido

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Cart extends Component {
   render() {
-    const { cartProducts } = this.props;
+    const { cartProducts, handleClick, handleDecrease, handleRemove } = this.props;
 
     const truth = cartProducts.length > 0;
     const warning = (
@@ -18,6 +18,7 @@ class Cart extends Component {
       <section className="cart-products">
         {cartProducts.filter((prod, index) => index === cartProducts
           .indexOf(cartProducts.find((item) => item.id === prod.id)))
+          .sort((a, b) => b.price - a.price)
           .map((prod) => (
             <div key={ prod.id }>
               <p data-testid="shopping-cart-product-name">{ prod.title }</p>
@@ -26,6 +27,30 @@ class Cart extends Component {
                   (item) => (item.id === prod.id),
                 ).length }
               </p>
+              <p>{ prod.price }</p>
+              <button
+                type="button"
+                id={ prod.id }
+                data-testid="product-decrease-quantity"
+                onClick={ () => handleDecrease(prod) }
+              >
+                -
+              </button>
+              <button
+                type="button"
+                id={ prod.id }
+                data-testid="product-increase-quantity"
+                onClick={ () => handleClick(prod) }
+              >
+                +
+              </button>
+              <button
+                type="button"
+                id={ prod.id }
+                onClick={ () => handleRemove(prod) }
+              >
+                Remover
+              </button>
             </div>
           ))}
       </section>
@@ -41,6 +66,9 @@ class Cart extends Component {
 
 Cart.propTypes = {
   cartProducts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleClick: PropTypes.func.isRequired,
+  handleDecrease: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired,
 };
 
 export default Cart;
--- src/services/api.js
+++ src/services/api.js
@@ -18,9 +18,3 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const json = await response.json();
   return json;
 }
-
-export async function getProduct(productId) {
-  const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
-  const json = await response.json();
-  return json;
-}
